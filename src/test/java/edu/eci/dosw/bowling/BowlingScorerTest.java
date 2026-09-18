5fc3fc140322a3ee47e1d14113f10add984b8d31
Santiago Andres Garcia Almario <santiago.garcia-a@mail.escuelaing.edu.co>
2026-09-17 22:35:03 -0500

    test: RED - B1 y B2 - score suma directa sin bonos

---
+++ src/test/java/edu/eci/dosw/bowling/BowlingScorerTest.java
@@ -0,0 +1,35 @@
+package edu.eci.dosw.bowling;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BowlingScorerTest {
+
+    private void rollMany(BowlingGame game, int times, int pins) {
+        for (int i = 0; i < times; i++) {
+            game.roll(pins);
+        }
+    }
+
+    @Test
+    @DisplayName("B1: juego con todos los tiros a 0 - score debe ser 0")
+    void allZeros_scoresZero() {
+        BowlingGame game = new BowlingGame();
+        rollMany(game, 20, 0);
+
+        assertEquals(0, game.score());
+    }
+
+    @Test
+    @DisplayName("B2: juego sin strikes ni spares - score es la suma directa de los pinos")
+    void noStrikesNoSpares_scoresSumOfPins() {
+        BowlingGame game = new BowlingGame();
+        for (int i = 0; i < 10; i++) {
+            game.roll(3);
+            game.roll(4);
+        }
+
+        assertEquals(70, game.score());
+    }
+}
